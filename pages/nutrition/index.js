c103aa64962af1dfae7bd9ffa08888e768ebbca6
bfamchon <bfamchon@cutii.io>
2021-01-20 23:29:31 +0100

    seo improvements

--- pages/nutrition/index.js
+++ pages/nutrition/index.js
@@ -11,7 +11,7 @@ export default function AdventureRecipes({ recipes }) {
     return (
         <>
             <NextSeo
-                title="La nutrition et recettes dédiées aux sportifs | Cook For Adventure"
+                title="Conseils nutrition et recettes dédiés au sport | Cook For Adventure"
                 description="Vous retrouverez ici tous les conseils en nutrition et recettes dédiées à l'effort !"
                 canonical={`https://cookforadventure.com/nutrition`}
                 openGraph={{
