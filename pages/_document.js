dc42ff9b4e4ad39ae5a4337b44422cd9abff1499
bfamchon <bfamchon@gmail.com>
2021-01-20 09:29:04 +0100

    ::sparkles: new favicon

--- pages/_document.js
+++ pages/_document.js
@@ -38,6 +38,14 @@ export default class MyDocument extends Document {
               'fr'
             }>
             <Head>
+              <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=xQ7PaK5rdG" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=xQ7PaK5rdG" />
+              <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=xQ7PaK5rdG" />
+              <link rel="manifest" href="/site.webmanifest?v=xQ7PaK5rdG" />
+              <link rel="mask-icon" href="/safari-pinned-tab.svg?v=xQ7PaK5rdG" color="#c65817" />
+              <link rel="shortcut icon" href="/favicon.ico?v=xQ7PaK5rdG" />
+              <meta name="msapplication-TileColor" content="#da532c" />
+              <meta name="theme-color" content="#ffffff" />
               {/* enable analytics script only for production */}
               {isProduction && (
                 <>
