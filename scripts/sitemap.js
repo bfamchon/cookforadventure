9663131ad0e847d7f8fd064fec290d78d97492a9
bfamchon <bfamchon@cutii.io>
2021-02-15 14:57:13 +0100

    optim seo

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -1,5 +1,18 @@
 const fs = require('fs');
 const globby = require('globby');
+const { url } = require('inspector');
+
+const getPriority = (url) => {
+  const splitedURLLength = url.split('/').length
+  switch (splitedURLLength) {
+    case 3:
+      return 1.00;
+    case 4:
+      return 0.80;
+    case 5:
+      return 0.70;
+  }
+}
 
 const generateSiteMap = async () => {
   const pages = await globby([
@@ -10,7 +23,7 @@ const generateSiteMap = async () => {
     'contents/**/*.md',
   ]);
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       ${pages
         .map(page => {
           const path = page
@@ -19,8 +32,13 @@ const generateSiteMap = async () => {
             .replace('.js', '')
             .replace('.md', '')
             .replace('/index', '');
+          let urlLoc = `https://cookforadventure.com${path}`;
+          if (urlLoc.charAt(urlLoc.length - 1) === '/') {
+            urlLoc.slice(0, -1);
+          }
           return `<url>
-            <loc>https://cookforadventure.com${path}</loc>
+            <loc>${urlLoc}</loc>
+            <priority>${getPriority(urlLoc)}</priority>
           </url>
           `;
         })
